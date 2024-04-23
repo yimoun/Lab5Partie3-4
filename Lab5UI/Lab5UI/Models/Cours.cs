b32364bcb0006b53ab13422bcab24bea25e4d375
Nelson Junior Yimou Noubissi <2130331@etu.cchic.ca>
2024-04-23 16:22:32 -0400

    premiers pas de l'application UI

---
+++ Lab5UI/Lab5UI/Models/Cours.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5UI.Models
+{
+    class Cours
+    {
+        [JsonProperty(PropertyName = "sigleCours")]
+        public string SigleCours { get; set; }
+
+
+        [JsonProperty(PropertyName = "titreCours")]
+        public string TitreCours { get; set; }
+
+
+        [JsonProperty(PropertyName = "dureeCours")]
+        public int DureeCours { get; set; }
+    }
+}
---
+++ Lab5UI/Lab5UI/Models/Lab5Processor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5UI.Models
+{
+    internal class Lab5Processor
+    {
+        /*API DOC:
+         */
+    }
+}
