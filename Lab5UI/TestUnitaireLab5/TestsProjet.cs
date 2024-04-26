0ddbe515d51b0f36eabf18abfc2e46fb53182b2e
Samara Boudreault <2230912@etu.cchic.ca>
2024-04-26 14:35:14 -0400

    faire un pull

---
+++ Lab5UI/TestUnitaireLab5/TestsProjet.cs
@@ -0,0 +1,14 @@
+global using Xunit;
+using Xunit.Abstractions;
+namespace TestUnitaireLab5
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+            //appel api ,deserialiser le get , comparer le deserialiser au ficher qu'on a crée si = test good si non test pas good 
+        }
+    }
+}
